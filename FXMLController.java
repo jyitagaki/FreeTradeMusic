46a35713de8c09a8607c1e1945beb10ea62ae943
Joshua Itagaki <jyitagaki@cpp.edu>
2017-04-19 13:45:36 -0700

    Controller Class

---
+++ FXMLController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
+public class FXMLController {
+
+	@FXML
+	private MenuItem signIn;
+	@FXML
+	private MenuItem createAcc;
+	@FXML
+	private Button homeButton;
+}
