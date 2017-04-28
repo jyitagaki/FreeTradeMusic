73ff1c0be0c839598206c775e1ee9e0325fa942e
Joshua Itagaki <jyitagaki@cpp.edu>
2017-04-27 18:27:02 -0700

    Create account controller

---
+++ CreateAccountController.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAccountController {
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private PasswordField confirmPass;
+
+    @FXML
+    private TextField usernameTxt;
+
+    @FXML
+    private TextField emailTxt;
+
+}
